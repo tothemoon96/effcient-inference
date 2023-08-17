586d0b70dfc1e1fc1514d93de7b6911df0cb3f48
Weiguang Han <hanweiguang1@gmail.com>
2023-08-18 00:49:01 +0800

    update

--- docker_run.sh
+++ docker_run.sh
@@ -3,22 +3,24 @@ export https_proxy=http://127.0.0.1:65530
 export http_proxy=http://127.0.0.1:65530
 export all_proxy=socks5://127.0.0.1:65530
 
-model=BELLE-2/BELLE-Llama2-13B-chat-0.4M
+model=facebook/opt-66b
 volume=$HF_HOME/hub
 
-# docker run --gpus all --shm-size 1g --network host \
-#     --rm \
-#     --env https_proxy=$https_proxy \
-#     --env http_proxy=$http_proxy \
-#     --env all_proxy=$all_proxy \
-#     -v $volume:/data \
-#     ghcr.io/huggingface/text-generation-inference:1.0.1 \
-#     --model-id $model
-
 docker run --gpus all --shm-size 1g --network host \
-    -it --rm \
+    --rm \
     --env https_proxy=$https_proxy \
     --env http_proxy=$http_proxy \
     --env all_proxy=$all_proxy \
     -v $volume:/data \
-    text_gen
+    ghcr.io/huggingface/text-generation-inference:1.0.1 \
+    --model-id $model
+
+# docker run --gpus all --shm-size 1g --network host \
+#     -it --rm \
+#     --env https_proxy=$https_proxy \
+#     --env http_proxy=$http_proxy \
+#     --env all_proxy=$all_proxy \
+#     -v $volume:/data \
+#     text_gen
+
+# text-generation-launcher
