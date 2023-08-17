057de9a634bedbf1d3a3083913e7b239884acd55
Weiguang Han <hanweiguang1@gmail.com>
2023-08-18 00:43:05 +0800

    update

--- docker_run.sh
+++ docker_run.sh
@@ -6,19 +6,19 @@ export all_proxy=socks5://127.0.0.1:65530
 model=BELLE-2/BELLE-Llama2-13B-chat-0.4M
 volume=$HF_HOME/hub
 
-docker run --gpus all --shm-size 1g --network host \
-    --rm \
-    --env https_proxy=$https_proxy \
-    --env http_proxy=$http_proxy \
-    --env all_proxy=$all_proxy \
-    -v $volume:/data \
-    ghcr.io/huggingface/text-generation-inference:1.0.1 \
-    --model-id $model
-
-# docker run --gpus all --shm-size 1g -p --network host \
-#     -it --rm \
+# docker run --gpus all --shm-size 1g --network host \
+#     --rm \
 #     --env https_proxy=$https_proxy \
 #     --env http_proxy=$http_proxy \
 #     --env all_proxy=$all_proxy \
 #     -v $volume:/data \
-#     text_gen
+#     ghcr.io/huggingface/text-generation-inference:1.0.1 \
+#     --model-id $model
+
+docker run --gpus all --shm-size 1g --network host \
+    -it --rm \
+    --env https_proxy=$https_proxy \
+    --env http_proxy=$http_proxy \
+    --env all_proxy=$all_proxy \
+    -v $volume:/data \
+    text_gen
